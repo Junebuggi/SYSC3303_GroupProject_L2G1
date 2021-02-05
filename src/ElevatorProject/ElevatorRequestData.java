5d78563a6e9e01a9731f15bce970e8f536aefb67
rutvikshah29 <rutviks20@gmail.com>
2021-02-05 02:47:40 -0500

    initial class for request data object

---
+++ src/ElevatorProject/ElevatorRequestData.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ElevatorProject;
+
+/**
+ * @author rutvikshah
+ *
+ */
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElevatorRequestData {
+	private Time time; //hh:mm:ss.mmm
+	private Integer floor; //current floor level
+	private Integer carButton; //destination floor
+	private String floorButton; //direction: "Up" or "Down"
+	
+	public ElevatorRequestData (Time time, Integer floor, Integer carButton, String floorButton) {
+		this.time = time;
+		this.floor = floor;
+		this.carButton = carButton;
+		this.floorButton = floorButton;
+	}
+}
